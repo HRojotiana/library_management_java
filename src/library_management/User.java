3a9e6343da97816c8827aa996c0c27d0d7cdd08d
HRojotiana <hei.rojotiana@gmail.com>
2023-11-18 16:49:04 +0300

    add class User

--- src/library_management/User.java
+++ src/library_management/User.java
@@ -1,2 +1,59 @@
-package library_management;public class User {
+package library_management;
+
+import java.util.Objects;
+
+public class User {
+    private String id;
+    private String name;
+    private String reference;
+
+    //Constructor
+
+    public User(String id, String name, String reference) {
+        this.id = id;
+        this.name = name;
+        this.reference = reference;
+    }
+
+    //Getter
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    //Setter
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    //Equals and hashcode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(getId(), user.getId()) && Objects.equals(getName(), user.getName()) && Objects.equals(getReference(), user.getReference());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getReference());
+    }
 }
