c7b3f0bb4056a2c3e3747c681d37abd2e267f565
HRojotiana <hei.rojotiana@gmail.com>
2023-11-18 16:44:49 +0300

    add Book class

---
+++ src/library_management/Book.java
@@ -0,0 +1,39 @@
+package library_management;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Book extends CrupdateBook{
+    String idAuthor;
+
+    public Book(String id, String bookName, int pageNumbers, String topic, Date releaseDate, String status, String idAuthor) {
+        super(id, bookName, pageNumbers, topic, releaseDate, status);
+        this.idAuthor = idAuthor;
+    }
+
+    //Getter
+
+    public String getIdAuthor() {
+        return idAuthor;
+    }
+
+    //Setter
+    public void setIdAuthor(String idAuthor) {
+        this.idAuthor = idAuthor;
+    }
+
+    //Equals and HashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+        if (!super.equals(o)) return false;
+        return Objects.equals(getIdAuthor(), book.getIdAuthor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getIdAuthor());
+    }
+}
