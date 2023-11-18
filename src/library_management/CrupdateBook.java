3e5c502daf0f4f7ad85921f10c074a3ba36dbdd2
HRojotiana <hei.rojotiana@gmail.com>
2023-11-18 16:27:08 +0300

    add CrupdateBook class

---
+++ src/library_management/CrupdateBook.java
@@ -0,0 +1,82 @@
+package library_management;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class CrupdateBook {
+    private String id;
+    private String bookName;
+    private int pageNumbers;
+    private String topic;
+    private Date releaseDate;
+    private String status;
+
+    public CrupdateBook(String id, String bookName, int pageNumbers, String topic, Date releaseDate, String status) {
+        this.id = id;
+        this.bookName = bookName;
+        this.pageNumbers = pageNumbers;
+        this.topic = topic;
+        this.releaseDate = releaseDate;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public int getPageNumbers() {
+        return pageNumbers;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public void setPageNumbers(int pageNumbers) {
+        this.pageNumbers = pageNumbers;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CrupdateBook that)) return false;
+        return getPageNumbers() == that.getPageNumbers() && Objects.equals(getId(), that.getId()) && Objects.equals(getBookName(), that.getBookName()) && Objects.equals(getTopic(), that.getTopic()) && Objects.equals(getReleaseDate(), that.getReleaseDate()) && Objects.equals(getStatus(), that.getStatus());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getBookName(), getPageNumbers(), getTopic(), getReleaseDate(), getStatus());
+    }
+}
