f31787b811776d2512c7cdf1a2ae85527a7e0eed
HRojotiana <hei.rojotiana@gmail.com>
2023-11-18 19:23:06 +0300

    fix create_table_subscribers: add birthdate for age restriction

--- src/library_management/scripts_sql/create_table_subscribers.sql
+++ src/library_management/scripts_sql/create_table_subscribers.sql
@@ -1,14 +1,15 @@
 CREATE TABLE IF NOT EXISTS "subscriber" (
     id VARCHAR(100) PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
+    birthdate DATE NOT NULL, -- for age restriction
     reference VARCHAR(100) NOT NULL
 );
 
-INSERT INTO "subscriber" (id, name, reference)
+INSERT INTO "subscriber" (id, name, birthdate, reference)
 VALUES ('subs1', 'Sophie Martin', 'jklk011');
 
-INSERT INTO "subscriber" (id, name, reference)
+INSERT INTO "subscriber" (id, name, birthdate, reference)
 VALUES ('subs2', 'Lucas Dupont','djkh232');
 
-INSERT INTO "subscriber" (id, name, reference)
+INSERT INTO "subscriber" (id, name,birthdate, reference)
 VALUES ('subs3', 'Marie Leblanc','mpol326');
