ee03b2e6ba38918e07a5ff5be5bc510f6c51bd5a
HRojotiana <hei.rojotiana@gmail.com>
2023-11-19 12:19:04 +0300

    start test in main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,39 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import library_management.Author;
+import library_management.AuthorCrudOperations;
+import library_management.BookCrudOperations;
+import library_management.Book;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        AuthorCrudOperations authorCrudOperations = new AuthorCrudOperations();
+        BookCrudOperations bookCrudOperations = new BookCrudOperations();
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //Insérer un nouvel auteur
+        Author newAuthor = new Author("auth67", "James Blue", "M");
+        Author savedAuthor = authorCrudOperations.save(newAuthor);
+        LOGGER.info("Author saved successfully"+savedAuthor.toString());
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        //Insérer une liste d'auteurs
+
+
+        //Afficher la liste de tous les auteurs
+        List<Author> authors = authorCrudOperations.findAll();
+        LOGGER.info("List of all authors: "+authors.toString());
+
+        //Insérer une nouveau livre
+        /*Book newBook = new Book("bk4", "Satan était un ange", 100, "other", Date.valueOf("2012-05-23"), "available", "auth1");
+        Book savedBook = bookCrudOperations.save(newBook);
+        LOGGER.info("Book saved successfully: "+savedBook.toString());*/
+
+        //Afficher la liste de tous les livres
+        /*List<Book> books = bookCrudOperations.findAll();
+        LOGGER.info("List of all books: "+books.toString());*/
     }
 }
